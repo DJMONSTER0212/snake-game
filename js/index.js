447633a363e8d269ba377023ac01be2d13610729
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2023-01-06 00:14:30 +0530

    using requestAnimationframe to render animation in our game

---
+++ js/index.js
@@ -0,0 +1,13 @@
+let direction = {x:0,y:0};
+let foodsound =  new Audio("mixkit-player-recharging-in-video-game-2041.wav");
+let gameover = new Audio("mixkit-arcade-retro-game-over-213.wav");
+let turn = new Audio("mixkit-quick-jump-arcade-game-239.wav");
+let running = new Audio("mixkit-arcade-retro-run-sound-220.wav");
+
+
+
+
+
+
+
+window.requestAnimationFrame(main);
